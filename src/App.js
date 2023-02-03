254f42630d7804a90841859f1ceafe6fcf984692
MDWebDevelopment <md.web.development404@gmail.com>
2023-02-03 18:33:38 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  function readFile() {
+    const textArea = document.querySelector(".code");
+    const input = document.querySelector(".files");
+    if (!input || !input.files) {
+      console.error("Nie znaleziono pliku do odczytania.");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function () {
+      console.log(reader.result);
+      textArea.innerHTML = reader.result;
+    };
+    reader.readAsText(input.files[0]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Files">
+        <input onChange={readFile} type="file" className="files"></input>
+      </div>
+      <div className="Editor">
+        <textarea className="code"></textarea>
+      </div>
     </div>
   );
 }
